5882150b1cc1efade12d5f115f09df6ebdd7d887
Tomasz Taras <tomasztaras@outlook.com>
2020-01-15 19:53:57 +0100

    dodano skrypty FME

---
+++ skrypty/fme_workspaces/nodes_fme_script.bat
@@ -0,0 +1,7 @@
+"C:\Program Files\FME\fme.exe" E:\nodes_count.fmw --SourceDataset_POSTGIS "podyplomowka" --SourceDataset_POSTGIS_3 "podyplomowka" --DestDataset_POSTGRES "podyplomowka" --WHERE_CLAUSE_POSTGIS "1=1 limit<space>2000000<space>offset<space>0"
+		  
+"C:\Program Files\FME\fme.exe" E:\nodes_count.fmw --SourceDataset_POSTGIS "podyplomowka" --SourceDataset_POSTGIS_3 "podyplomowka" --DestDataset_POSTGRES "podyplomowka" --WHERE_CLAUSE_POSTGIS "1=1 limit<space>2000000<space>offset<space>2000000"
+
+"C:\Program Files\FME\fme.exe" E:\nodes_count.fmw --SourceDataset_POSTGIS "podyplomowka" --SourceDataset_POSTGIS_3 "podyplomowka" --DestDataset_POSTGRES "podyplomowka" --WHERE_CLAUSE_POSTGIS "1=1 limit<space>2000000<space>offset<space>4000000"
+
+"C:\Program Files\FME\fme.exe" E:\nodes_count.fmw --SourceDataset_POSTGIS "podyplomowka" --SourceDataset_POSTGIS_3 "podyplomowka" --DestDataset_POSTGRES "podyplomowka" --WHERE_CLAUSE_POSTGIS "1=1 limit<space>2000000<space>offset<space>6000000"
